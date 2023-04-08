b9e0917bf2436c602fe3a7947b32fbdeb7c015a8
weroszymaniak <weronika.szymaniak@hotmail.com>
2023-04-08 12:55:57 +0200

    changes in task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+let timerId = null;
+stopBtn.disabled = true;
 
+const randomColor = function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+const changeBodyColor = event => {
+  btnDisabled();
+  timerId = setInterval(() => {
+    body.style.backgroundColor = `${randomColor}`;
+  }, 1000);
+};
+
+const stopBodyColor = event => {
+  clearInterval(timerId);
+  btnDisabled();
+};
+
+function btnDisabled() {
+  if (!startBtn.disabled) {
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+  } else {
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+  }
+}
+
+startBtn.addEventListener('click', changeBodyColor());
+stopBtn.addEventListener('click', stopBodyColor());
