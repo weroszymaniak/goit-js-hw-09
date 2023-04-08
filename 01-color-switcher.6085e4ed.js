c1495e0f63387dd59e899114c16dc0d428b48613
weroszymaniak <116954355+weroszymaniak@users.noreply.github.com>
2023-04-08 10:56:22 +0000

    Deploying to gh-pages from @ weroszymaniak/goit-js-hw-09@b9e0917bf2436c602fe3a7947b32fbdeb7c015a8 🚀

---
+++ 01-color-switcher.6085e4ed.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),d=document.querySelector("body"),n=null;e.disabled=!0;var o=function(){return"#".concat(Math.floor(16777215*Math.random()).toString(16))};function a(){t.disabled?(t.disabled=!1,e.disabled=!0):(t.disabled=!0,e.disabled=!1)}t.addEventListener("click",(a(),void(n=setInterval((function(){d.style.backgroundColor="".concat(o)}),1e3)))),e.addEventListener("click",(clearInterval(n),void a()))}();
+//# sourceMappingURL=01-color-switcher.6085e4ed.js.map
